5f045c82f87835ffe63705661a0ea76ceef8bc89
Mateusz Tomczyk <tulfm@poczta.fm>
2017-02-07 22:03:15 +0100

    disabling visualization when window is inactive

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+// const renderer = electron.ipcRenderer;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -21,12 +22,22 @@ function createWindow () {
   mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  // TODO those should be show and hide but i3 doesn't trigger them when
+  // switching workspace.
+  mainWindow.on('focus', () => {
+    mainWindow.webContents.send('show');
+  });
+
+  mainWindow.on('blur', () => {
+    mainWindow.webContents.send('hide');
+  });
 }
 
 // This method will be called when Electron has finished
@@ -35,7 +46,7 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -43,13 +54,10 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
