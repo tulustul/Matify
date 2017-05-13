19f66a38f2684d53bbe289902e0ff39c9bfcd490
Mateusz Tomczyk <tulfm@poczta.fm>
2017-05-13 15:49:26 +0200

    much improved youtube support including seeking

--- proxy_server/youtube_proxy.py
+++ proxy_server/youtube_proxy.py
@@ -1,34 +1,11 @@
 import pafy
 
-from flask import Response
+from flask import redirect
 
 from app import app
-
 
 @app.route('/youtube/<string:track>', methods=['GET'])
 def youtube_proxy(track):
   url = 'https://www.youtube.com/watch?v={}'.format(track)
-  video = pafy.new(url)
-  audiostream = video.getbestaudio()
-
-  def generate():
-    audiostream.download('audio.tmp', quiet=True)
-    with open('audio.tmp', 'rb') as audiofile:
-      with app.test_request_context():
-        from flask import request
-        range_ = request.headers.get('Range')
-        if range_:
-          range_ = range_.split('=')[1][:-1]
-          audiofile.seek(range_)
-      data = audiofile.read(1024)
-      while data:
-        yield data
-        data = audiofile.read(1024)
-
-  return Response(
-    generate(),
-    mimetype='audio/{}'.format(audiostream.extension),
-    headers={
-      'Accept-Ranges': 'bytes',
-    },
-  )
+  video = pafy.new(url, basic=False)
+  return redirect(video.getbestaudio().url)
